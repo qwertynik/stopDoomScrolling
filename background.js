c2b12f492ace7becf26b7b9a75becf4a42a04ad1
nikhilranka <6836536+qwertynik@users.noreply.github.com>
2025-08-20 17:48:49 +0530

    - adding additional default messages to show. - removing excessive permissions.

--- background.js
+++ background.js
@@ -1,18 +1,22 @@
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
-    trackedSites: ["x.com","linkedin.com","instagram.com"],
-    blockTime: 20,
-    messages: ["Do you really want to scroll further?"],
-    showTimer: true
-  });
+    chrome.storage.sync.set({
+        trackedSites: ["x.com", "linkedin.com", "instagram.com"],
+        blockTime: 20,
+        messages: [
+            "Do you want to continue scrolling?",
+            "Are your scrolling intentionally?",
+            "Do you want to take a break instead?",
+            "In what other way you can actively engage yourself?",
+        ],
+        showTimer: true
+    });
 });
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "toggleTimer") {
-    chrome.tabs.query({}, (tabs) => {
-      for (const tab of tabs) {
-        if (tab.id) chrome.tabs.sendMessage(tab.id, { type: "toggleTimer", show: message.show });
-      }
-    });
-  }
+    if (message.type === "toggleTimer") {
+        chrome.tabs.query({}, (tabs) => {
+            for (const tab of tabs) {
+                if (tab.id) chrome.tabs.sendMessage(tab.id, {type: "toggleTimer", show: message.show});
+            }
+        });
+    }
 });
